f0cba355f7984d9f137af9c14e65dab416d4b7f3
Henry Skalla <hcskalla@gmail.com>
2026-02-17 19:37:11 -0600

    game logging added

--- league/views.py
+++ league/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.conf import settings
 
-from .models import League
+from .models import League, Game
+from django.contrib.auth.models import User
 
 def index(request):
     if request.user.is_authenticated:
@@ -15,7 +16,28 @@ def index(request):
 @login_required
 def league(request, slug):
     l = get_object_or_404(League, slug=slug)
-    return render(request, "league/league.html", {"league": l})
+    u = request.user
+    # parse form submission
+    if request.method == "POST":
+        if 'log_game_submit' in request.POST:
+            game_agent = u
+            game_patient = User.objects.get(username=request.POST.get("opponent"))
+            if request.POST.get("win"):
+                game_win = True
+            else:
+                game_win = False
+            # print(game_agent," (",type(game_agent),") ",game_patient," (",type(game_patient),") ",game_win, " (",type(game_win),")")
+            game = Game(agent=game_agent,patient=game_patient,league=l,win=game_win)
+            game.save()
+        elif 'confirm_game_submit' in request.POST:
+            game = Game.objects.get(id=request.POST.get("game"))
+            # print(request.POST.get("game")," (",type(request.POST.get("game")),") ",request.POST.get("confirm_game_submit")," (",type(request.POST.get("confirm_game_submit")),")")
+            if request.POST.get("confirm_game_submit") == "confirm":
+                game.confirmed = True
+                game.save()
+            else:
+                game.delete()
+    return render(request, "league/league.html", {"league": l, "user": u})
 
 @login_required
 def join(request, slug):
